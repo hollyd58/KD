ebcd1eace6d39cfa43c228756ba7ccd98d0cc8a5
kendra <kendra.dunham@gmail.com>
2016-04-29 08:30:05 -0700

    first commit

---
+++ src/main/java/Word.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Word {
+  private static ArrayList<Word> theWords = new ArrayList<Category>();
+
+  private String mWord;
+
+  public Word(String word){
+    mWord = word;
+  }
+
+}
