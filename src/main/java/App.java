4471b4343f431c35927420a7e3279b273c90ed8c
kendra <kendra.dunham@gmail.com>
2016-04-29 12:36:59 -0700

    began front-end, basic add word functionality added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,8 +7,43 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    // String layout = "templates/layout.vtl";
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request,response) -> {
+      HashMap<String,Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model,layout);
+    }, new VelocityTemplateEngine());
+
+    get("/index/new", (request, response) -> {
+      HashMap<String,Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/new-word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/index", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String word = request.queryParams("newWords");
+      Word newWord = new Word(word);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/index", (request, response) ->{
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("words", Word.getWords());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("words/:id", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Word word = Word.find(Integer.parseInt(request.params(":id")));
+      model.put("word", word);
+      model.put("template", "templates/word.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
     //
     // get("/", (request, response) -> {
     //   Map<String, Object> model = new HashMap<String, Object>();
